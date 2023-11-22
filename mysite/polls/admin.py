cbf1ae8d342006b1b7b7f20aba622c4b0b473c47
Ja <jarek.ozieblo@wp.pl>
2023-11-22 16:23:46 +0100

    update

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import  Osoba, Stanowisko
 
-admin.site.register(Osoba)
+
+
+class OsobaAdmin(admin.ModelAdmin):
+    list_display = ('imie', 'nazwisko', 'plec', 'stanowisko', 'data_dodania')
+    list_filter = ('stanowisko', 'data_dodania')
+
+    @admin.display(description='Stanowisko')
+    def stanowisko(self, obj):
+        if obj.stanowisko:
+            return f"{obj.stanowisko} ({obj.stanowisko.id})"
+        else:
+            return None
+
+admin.site.register(Osoba, OsobaAdmin)
 admin.site.register(Stanowisko)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -12,13 +12,20 @@ class Stanowisko(models.Model):
         return f"{self.nazwa}"
 
 class Osoba(models.Model):
-
     imie = models.CharField(max_length=50)
     nazwisko = models.CharField(max_length=70)
     plec = models.IntegerField(choices=plec.choices)
     stanowisko = models.ForeignKey(Stanowisko, null=True, blank=True, on_delete=models.SET_NULL)
+    data_dodania = models.DateField(auto_now_add=True, editable=False)
+    class Meta:
+        ordering= ["nazwisko"]
+
+
+    def show(self):
+        return f"{self.imie} {self.nazwisko}"
 
     def __str__(self):
-        return f"{self.imie}"
+        return self.show()
+
 
 
