9fa334e71f4bb648ae19befd8e34354f7515a133
Ja <jarek.ozieblo@wp.pl>
2023-11-23 12:16:53 +0100

    update

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
 
 plec = models.IntegerChoices('Płeć', "Kobieta Mężczyzna Inne")
 
+def validate_alpha(value):
+    if not value.isalpha():
+        raise ValidationError('Nazwa może zawierać tylko litery.')
 
+def validate_past_date(value):
+    if value > timezone.now():
+        raise ValidationError('Data dodania nie może być z przyszłości.')
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length=50)
     opis = models.CharField(max_length=50, null=True, blank=True)
@@ -12,11 +21,11 @@ class Stanowisko(models.Model):
         return f"{self.nazwa}"
 
 class Osoba(models.Model):
-    imie = models.CharField(max_length=50)
-    nazwisko = models.CharField(max_length=70)
+    imie = models.CharField(max_length=50, validators=[validate_alpha])
+    nazwisko = models.CharField(max_length=70, validators=[validate_alpha])
     plec = models.IntegerField(choices=plec.choices)
     stanowisko = models.ForeignKey(Stanowisko, null=True, blank=True, on_delete=models.SET_NULL)
-    data_dodania = models.DateField(auto_now_add=True, editable=False)
+    data_dodania = models.DateTimeField(default=timezone.now, validators=[validate_past_date])
     class Meta:
         ordering= ["nazwisko"]
 
